250dc21bfc12320cfa79694ee6f842b4c1d1b5ea
Luiz Daniel Ramos Franca <luizdaniel.r.f@gmail.com>
2017-06-17 15:57:17 -0300

    first commit

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: luizdaniel
+ */
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+
+	cout << "template" << endl;
+
+	return 0;
+}
+
